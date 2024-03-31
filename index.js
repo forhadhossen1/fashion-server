de97dd118b46ce0d4a29de53dca63324222b2439
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-31 15:17:47 +0600

    get product data system includ

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 //middleware
 app.use(cors());
@@ -12,7 +14,6 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yktx4zp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,7 +28,18 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        await client.connect();
+
+
+        const productCollection = client.db('fashionDB').collection('product')
+
+        // productCollection related code ... 
+        app.get('/product', async (req, res) => {
+            const result = await productCollection.find().toArray();
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
