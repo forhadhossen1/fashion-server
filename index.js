54f4a9b3c3812066e7c742b603fb473775819bd1
forhadhossen1 <mr.forhad1030@gmail.com>
2024-04-21 11:30:44 +0600

    veryfiy token

--- index.js
+++ index.js
@@ -45,9 +45,20 @@ async function run() {
         })
 
         //middlewares
-        const verifyToken = (req, res, next)
-        console.log('inside verify token', req.headers);
-        next();
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbidden access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidden access' })
+                }
+                req.decodded = decodded;
+                next();
+            })
+        }
 
         // usersCollection related api code 
         app.post('/users', verifyToken, async (req, res) => {
